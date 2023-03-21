d4b9cee6e797da8cefc9a99832e43699a8acdcb5
Max Jacobsen <mumse999@outlook.dk>
2023-03-21 13:26:57 +0100

    main

--- HangmanServer/Program.cs
+++ HangmanServer/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Hangman
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
